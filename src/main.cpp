abe1b45b520650cce8f81dd6c8e3925040d3ea4d
Jonathan H <124759864+jonathan-herring@users.noreply.github.com>
2023-11-28 16:25:48 -0800

    Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <GL/glut.h>
 
 
+void display(); // Display callback
+// Make a reshape callback function if worried about user resizing the window
 
-void display(); // Display callback function
-
-
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0); // If we ever want to chang the background color
+}
 
 int main(int argc, char** argv)
 {
@@ -14,16 +17,17 @@ int main(int argc, char** argv)
     glutInitWindowSize(500, 500);
     glutCreateWindow("Undeath Sentence");
     glutDisplayFunc(display); // Argument display is a callback function that updates the display
+    
+    init();
 
     glutMainLoop(); // Takes care of all interactions with program and calls callback functions
 
     return 0;
 }
 
-
-
 void display() // Updates display
 {
+    glClear(GL_COLOR_BUFFER_BIT);
     glutSwapBuffers(); // Displays the frame buffer on the screen
 }
 
