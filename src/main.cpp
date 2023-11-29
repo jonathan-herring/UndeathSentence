32d541e513b4c44d5eacd38faf65dfa5fb26b079
Jonathan H <124759864+jonathan-herring@users.noreply.github.com>
2023-11-28 18:02:40 -0800

    Add player movement and input handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,88 @@
+#include "player.h"
+
 #include <GL/glut.h>
 
+Player player;
 
 void display(); // Display callback
 void reshape(int, int); // Reshape callback
+void update(int);
+void handleKeypress(unsigned char, int, int); // Keypress callback
 
 void init()
 {
     glClearColor(0.0, 0.0, 0.0, 1.0); // If we ever want to chang the background color
 }
 
+
+
+
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-    glutInitWindowPosition(200, 100);
-    glutInitWindowSize(500, 500);
+    // glutInitWindowPosition(200, 100);
+    glutInitWindowSize(800, 600);
     glutCreateWindow("Undeath Sentence");
+
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutKeyboardFunc(handleKeypress);
+    glutTimerFunc(60, update, 0);
     
-    init();
+
 
     glutMainLoop(); // Takes care of all interactions with program and calls callback functions
 
     return 0;
 }
 
+
+
+
+
 void display() // Updates display
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    player.draw();
     glutSwapBuffers(); // Displays the frame buffer on the screen
 }
+
 
 void reshape(int width, int height)
 {
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 40.0, 0.0, 40.0, -1.0, 1.0);
+    glOrtho(0, 1, 0, 1, -1, 1);
     glMatrixMode(GL_MODELVIEW);
+}
+
+
+void update(int value)
+{
+    glutPostRedisplay();
+    glutTimerFunc(33, update, 0);
+}
+
+
+void handleKeypress(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 'w':
+            player.move(0, 0.01f);
+            break;
+        case 's':
+            player.move(0, -0.01f);
+            break;
+        case 'a':
+            player.move(-0.01f, 0);
+            break;
+        case 'd':
+            player.move(0.01f, 0);
+            break;
+    }
 }
 
