fd7ae1dcc2dfb75d23ae4a4cb10b583b78a5bcab
Nathaniel Mitter <130000100+SexyJesusFreak@users.noreply.github.com>
2023-11-30 17:28:20 -0800

    added player.cpp

---
+++ src/player.cpp
@@ -0,0 +1,27 @@
+#include "player.h"
+
+#include <GL/glut.h>
+
+Player::Player() 
+{
+    this->x = 0.5f;
+    this->y = 0.5f;
+    this->width = 0.05f;
+    this->height = 0.05f;
+}
+
+void Player::draw()
+{
+    glBegin(GL_QUADS);
+        glVertex2f(x - width / 2, y - height / 2);
+        glVertex2f(x + width / 2, y - height / 2);
+        glVertex2f(x + width / 2, y + height / 2);
+        glVertex2f(x - width / 2, y + height / 2);
+    glEnd();
+}
+
+void Player::move(float dx, float dy) 
+{
+    this->x += dx;
+    this->y += dy;
+}
